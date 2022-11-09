7998c745f718e5b45da2b7aa6426755285e2ca4d
Jenna Smith <175330+jjenzz@users.noreply.github.com>
2022-11-09 15:22:48 +0000

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import * as ReactNative from "react-native";
+import * as GestureHandler from "react-native-gesture-handler";
+import Animated, * as Reanimated from "react-native-reanimated";
 
 export default function App() {
+  const [open, setOpen] = React.useState(false);
+  const translateY = Reanimated.useSharedValue(0);
+
+  const handleGestureEvent = Reanimated.useAnimatedGestureHandler({
+    onActive: (event) => (translateY.value = event.translationY),
+    onEnd: (event) => {
+      Reanimated.runOnJS(alert)(event.velocityY);
+      translateY.value = Reanimated.withSpring(0, null, () => {
+        return Reanimated.runOnJS(setOpen)(false);
+      });
+    },
+  });
+
+  const boxStyle = Reanimated.useAnimatedStyle(() => ({
+    transform: [{ translateY: translateY.value }],
+  }));
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandler.GestureHandlerRootView
+      style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+    >
+      {open && (
+        <GestureHandler.PanGestureHandler onGestureEvent={handleGestureEvent}>
+          <Animated.View
+            style={[
+              boxStyle,
+              { width: 300, height: 300, backgroundColor: "red" },
+            ]}
+          />
+        </GestureHandler.PanGestureHandler>
+      )}
+      <ReactNative.Button title="Open" onPress={() => setOpen(true)} />
+    </GestureHandler.GestureHandlerRootView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
